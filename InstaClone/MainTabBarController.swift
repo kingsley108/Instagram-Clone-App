8ce5ade25cc1b97cd5c6f71e560d2de3154995fb
Kingsley Charles <kingsley.charles@global.com>
2021-01-15 07:52:45 +0000

    Ep 5 Main Tab UI and Fetch User

---
+++ InstaClone/MainTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarController.swift
+//  InstaClone
+//
+//  Created by Kingsley Charles on 15/01/2021.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
