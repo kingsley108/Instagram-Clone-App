3bd444bc61da77855fe6ec389eb914e72b14fe64
Kingsley Charles <kingsley.charles@global.com>
2021-01-22 09:39:22 +0000

    Last Checkpoint where most things work still photoClass issues

---
+++ InstaClone/Models/Posts.swift
@@ -0,0 +1,8 @@
+//
+//  Struct.swift
+//  InstaClone
+//
+//  Created by Kingsley Charles on 22/01/2021.
+//
+
+import Foundation
---
+++ InstaClone/UserProfile/UserPostsCell.swift
@@ -0,0 +1,12 @@
+//
+//  UserPostsCell.swift
+//  InstaClone
+//
+//  Created by Kingsley Charles on 22/01/2021.
+//
+
+import UIKit
+
+class UserPostsCell: UICollectionViewCell {
+    
+}
